10bb49b6c305b56205e7725fbdb140240d846211
Dia Seu <diana.seung@gmail.com>
2021-11-03 13:52:56 -0700

    vue init, import componenets and app.vue code

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,131 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <Header @toggle-add-task="toggleAddTask" title="Daily Task App" 
+      :showAddTask="showAddTask"
+    />
+    <div v-show="showAddTask">
+      <AddTask @add-task="addTask" />
+    </div>
+    <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks" />
+  </div>
+  <!-- <HelloWorld msg="Daily Task App"/> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Tasks,
+    AddTask
+  },
+  data() {
+    return {
+      tasks: [],
+      // use lifecycle method to hook - created()
+      showAddTask: false
+    }
+  },
+  methods: {
+    toggleAddTask() {
+      this.showAddTask = !this.showAddTask
+    },
+    addTask(task) {
+      this.tasks = [...this.tasks, task]
+    },
+    deleteTask(id) {
+      // console.log('task', id)
+      if (confirm('are you sure to want to delete this tasK?')) {
+        this.tasks = this.tasks.filter(() => task.id !== id)
+      }
+    },
+    toggleReminder(id) {
+      this.tasks = this.tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task
+      )
+    }
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: 'Eye Follow-up',
+        day: 'Mon Nov 1 at 4:30pm',
+        reminder: false,
+      },
+      {
+        id: 2,
+        text: 'Vivosun Tent - Garden Grove',
+        day: 'Tues Nov 2 at 12pm',
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: 'Bipen & Ghost - Long Beach',
+        day: 'Wed Nov 3 at 2pm',
+        reminder: true,
+      },
+      {
+        id: 4,
+        text: 'Milsbo & Plants - Thousand Oaks',
+        day: 'Thurs Nov 4 at 11am',
+        reminder: true,
+      },
+      {
+        id: 5,
+        text: 'Monstera Esquelito Node - Westminster',
+        day: 'Sat Nov 6 at 9am',
+        reminder: false,
+      },
+
+    ]
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: 'Poppins', sans-serif;
+}
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+.btn:focus {
+  outline: none;
+}
+.btn:active {
+  transform: scale(0.98);
+}
+.btn-block {
+  display: block;
+  width: 100%;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <!-- <h2>Task Tracker</h2> -->
+    <!-- <img alt="Vue logo" src="../assets/logo.png" style="max-width: 50px;"> -->
+    <Button @btn-click="$emit('toggle-add-task')" :text="showAddTask ? 'Close' : 'Add Task'" :color="showAddTask ? 'red' : 'green'" />
+  </header>
+</template>
+
+<script>
+import Button from './Button'
+
+export default {
+  name: 'Header',
+  components: {
+    Button
+  },
+  props: {
+    title: String,
+    showAddTask: Boolean
+  },
+}
+</script>
+
+<style scoped>
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+  }
+</style>
